98b657d68d485adebd311eb5aad348a2b0f9f76b
Jacob Rendina <jrendina@adobe.com>
2026-06-04 16:13:11 +1000

    WOOHOO! BC mounted in the right location

--- blocks/brand-concierge/brand-concierge.js
+++ blocks/brand-concierge/brand-concierge.js
@@ -2,4 +2,11 @@ export default function decorate(block) {
   const mount = document.createElement('div');
   mount.setAttribute('id', 'brand-concierge-mount');
   block.append(mount);
+
+  window.adobe.concierge.bootstrap({
+    instanceName: "alloy",
+    stylingConfigurations: window.styleConfiguration,
+    selector: "#brand-concierge-mount",
+    stickySession: false
+  });
 }

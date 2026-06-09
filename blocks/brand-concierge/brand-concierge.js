308aaf4a332d4edb700226a26b34c8c7fe403840
Jacob Rendina <jrendina@adobe.com>
2026-06-09 13:23:10 +1000

    Simplifying repo further

--- blocks/brand-concierge/brand-concierge.js
+++ blocks/brand-concierge/brand-concierge.js
@@ -1,16 +1,10 @@
-import { loadCSS } from '../../scripts/aem.js';
-
-/**
- * Brand Concierge block.
- * Makes the block the agent mount point and starts Brand Concierge.
- * @param {Element} block the block element
- */
 export default function decorate(block) {
-  block.id = 'brand-concierge-mount';
+  block.setAttribute('id', 'brand-concierge-mount');
+
   window.adobe.concierge.bootstrap({
-    instanceName: 'alloy',
+    instanceName: "alloy",
     stylingConfigurations: window.styleConfiguration,
-    selector: '#brand-concierge-mount',
-    stickySession: false,
+    selector: "#brand-concierge-mount",
+    stickySession: false
   });
 }
